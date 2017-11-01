098490e72eff12499490ab32adea16328ea1ccf1
Stoyan Penchev <spenchev@gmail.com>
2017-11-01 15:13:20 +0200

    in progress FillMatrix

---
+++ FillMatrix/FillMatrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class FillMatrix
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char type = char.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+        int counter = 0;
+
+        if (type == 'a')
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    matrix[row, col] = counter + 1;
+                    counter++;
+                }
+            }
+        }
+        else if (type == 'b')
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (col % 2 == 0)
+                {
+                    for (int row = 0; row < matrix.GetLength(0); row++)
+                    {
+                        matrix[row, col] = counter + 1;
+                        counter++;
+                    }
+                }
+                else
+                {
+                    for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+                    {
+                        matrix[row, col] = counter + 1;
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
